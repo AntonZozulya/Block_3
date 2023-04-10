a4f29e2eb35f3503f5f69c6d97300484604049f4
AntonZozulya <Antonzo93@mail.ru>
2023-04-10 03:21:53 +0300

    add task demo

---
+++ 0002)_Seminar_1_Task_demo/Program.cs
@@ -0,0 +1,6 @@
+﻿// Необходимо написать программу, которая принемает число на вход и показывает его квадрат
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(n * n);
