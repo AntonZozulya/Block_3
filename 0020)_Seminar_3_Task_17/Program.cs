adcea6f47666a30f17f68df8271541add3f01ca8
AntonZozulya <Antonzo93@mail.ru>
2023-04-11 02:18:31 +0300

    add task 17

---
+++ 0020)_Seminar_3_Task_17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Напишите программу, которая принемает на вход 
+// кординаты точки (Х и У) причем Х не равен 0 и У не равен 0
+// и выдает номер четверти плоскости, в которой находится эта точка
+
+Console.Clear();
+Console.Write("Введите координату X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+while (x == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату X: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату Y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
