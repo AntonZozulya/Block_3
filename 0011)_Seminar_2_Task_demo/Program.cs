18b34c2ee69722b774fb50a1d79045af9130ef97
AntonZozulya <Antonzo93@mail.ru>
2023-04-10 03:29:19 +0300

    add task demo

---
+++ 0011)_Seminar_2_Task_demo/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Напишите программау, которая выводит случайное число
+// [10, 99] и показывает наиольшую цифру числа
+
+Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine($"Случайное сгенерированное число {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
