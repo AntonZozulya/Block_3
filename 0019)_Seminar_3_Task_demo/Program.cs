999f8c80dd40e6ed20333a794601b4e21ae2c510
AntonZozulya <Antonzo93@mail.ru>
2023-04-11 01:55:57 +0300

    add task demo

---
+++ 0019)_Seminar_3_Task_demo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// Напишите программу, которая будет находить сумму цифр числа
+
+Console.Clear();
+string s = Console.ReadLine()!;
+int sum = 0;
+
+for (int i = 0; i < s.Length; i++)
+    sum = sum + int.Parse(s[i].ToString());
+Console.WriteLine(sum);
