a88064cadd84645059da1ee4619654b148a3be97
AntonZozulya <Antonzo93@mail.ru>
2023-04-11 03:14:18 +0300

    add task 22

---
+++ 0023)_Seminar_3_Task_22/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Напишите программу которая принемает на вход 
+// число N и выдает таблицу квадратов от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} ^ 2 = {i * i}");
